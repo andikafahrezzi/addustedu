d4494b14b48aed6a9c31ce1b1ea2a4cfc183163c
addust <andikafahrezi5@gmail.com>
2025-03-27 21:05:37 +0700

    perf: remove irrelevant files and functions

--- application/controllers/Materi.php
+++ application/controllers/Materi.php
@@ -11,21 +11,6 @@ class Materi extends CI_Controller
         $this->load->library('form_validation');
         $this->load->library('disqus');
         $this->load->model('M_materi');
-        $this->list_materi['matematika_x'] = $this->M_materi->matematika_x()->result();
-        $this->list_materi['matematika_xi'] = $this->M_materi->matematika_xi()->result();
-        $this->list_materi['matematika_xii'] = $this->M_materi->matematika_xii()->result();
-        $this->list_materi['ipa_x'] = $this->M_materi->ipa_x()->result();
-        $this->list_materi['ipa_xi'] = $this->M_materi->ipa_xi()->result();
-        $this->list_materi['ipa_xii'] = $this->M_materi->ipa_xii()->result();
-        $this->list_materi['indo_x'] = $this->M_materi->indo_x()->result();
-        $this->list_materi['indo_xi'] = $this->M_materi->indo_xi()->result();
-        $this->list_materi['indo_xii'] = $this->M_materi->indo_xii()->result();
-        $this->list_materi['inggris_x'] = $this->M_materi->inggris_x()->result();
-        $this->list_materi['inggris_xi'] = $this->M_materi->inggris_xi()->result();
-        $this->list_materi['inggris_xii'] = $this->M_materi->inggris_xii()->result();
-        $this->list_materi['agama_x'] = $this->M_materi->agama_x()->result();
-        $this->list_materi['agama_xi'] = $this->M_materi->agama_xi()->result();
-        $this->list_materi['agama_xii'] = $this->M_materi->agama_xii()->result();
     }
     public function index() 
     {
@@ -68,80 +53,4 @@ class Materi extends CI_Controller
         $this->load->view('materi/navm');
         $this->load->view('materi/belajar', $data);
     }
-
-    public function matematika_x()
-    {
-        $this->generateMateri('matematika_x');
-    }
-    
-    public function matematika_xi()
-    {
-        $this->generateMateri('matematika_xi');
-    }
-
-    public function matematika_xii()
-    {
-        $this->generateMateri('matematika_xii');
-    }
-
-    public function ipa_x()
-    {
-        $this->generateMateri('ipa_x');
-    }
-
-    public function ipa_xi()
-    {
-        $this->generateMateri('ipa_xi');
-    }
-
-    public function ipa_xii()
-    {
-        $this->generateMateri('ipa_xii');
-    }
-
-    public function indo_x()
-    {
-        $this->generateMateri('indo_x');
-    }
-
-    public function indo_xi()
-    {
-        $this->generateMateri('indo_xi');
-    }
-
-    public function indo_xii()
-    {
-        $this->generateMateri('indo_xii');
-    }
-
-    public function inggris_x()
-    {
-        $this->generateMateri('inggris_x');
-    }
-    
-    public function inggris_xi()
-    {
-        $this->generateMateri('inggris_xi');
-    }
-    
-    public function inggris_xii()
-    {
-        $this->generateMateri('inggris_xii');
-    }
-    
-    public function agama_x()
-    {
-        $this->generateMateri('agama_x');
-    }
-    
-    public function agama_xi()
-    {
-        $this->generateMateri('agama_xi');
-    }
-    
-    public function agama_xii()
-    {
-        $this->generateMateri('agama_xii');
-    }
-
 }
