51b06e54d4cfa57385d8b899c00336e0ccc7d305
addust <andikafahrezi5@gmail.com>
2025-09-22 19:30:24 +0700

    style: change banner guru login

--- application/views/guru/login.php
+++ application/views/guru/login.php
@@ -84,7 +84,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                         </form>
                     </div>
                 </div>
-                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="<?= base_url('assets/') ?>stisla-assets/img/unsplash/ss.jpg">
+                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="<?= base_url('assets/') ?>stisla-assets/img/unsplash/foto.jpg">
                     <div class="absolute-bottom-left index-2">
                         <div class="text-light p-5 pb-2">
                             <div class="mb-5 pb-3">
