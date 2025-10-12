b2363dd0b2b55538fbedf0628a21b0f584fec42b
addust <andikafahrezi5@gmail.com>
2025-10-12 17:23:43 +0700

    perf: delete koneksi and autofil at root folder and admin folder

--- application/views/admin/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_addustedu");
-
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Koneksi database gagal : " . mysqli_connect_error();
-}
